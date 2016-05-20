6c0710afcc71626b493185a86f21f361c7a27e10
Hedhili Chouchene <hdili.chouchene@gmail.com>
2016-05-20 12:18:35 +0200

    check error dans les formulaires

--- app/src/main/java/fr/oqom/ouquonmange/models/Constants.java
+++ app/src/main/java/fr/oqom/ouquonmange/models/Constants.java
@@ -3,4 +3,8 @@ package fr.oqom.ouquonmange.models;
 public class Constants {
     public static String OQOM_BASE_URL = "http://ouquonmange.berezovskiy.fr";
     public static String REGEX_PASSWORD = "^[A-Za-z0-9]{6,20}$";
+    public static String REGEX_NAME_COMMUNITY = "^[A-Za-z]{4,20}$";
+    public static int MIN_LENGTH_NAME_COMMUNITY = 4;
+    public static int MAX_LENGTH_NAME_COMMUNITY = 30;
+
 }
