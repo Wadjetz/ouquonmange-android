4c647c6a978ca6a7c7cf4160119f521badfdab55
Hedhili Chouchene <hdili.chouchene@gmail.com>
2016-05-27 03:23:21 +0200

    ajouter datetimepicker pour le creation du events

--- app/src/main/java/fr/oqom/ouquonmange/CreateEventActivity.java
+++ app/src/main/java/fr/oqom/ouquonmange/CreateEventActivity.java
@@ -1,19 +1,88 @@
 package fr.oqom.ouquonmange;
 
+import java.util.Calendar;
+
+import DateTimePicker.DateTimePicker.DateWatcher;
+import DateTimePicker.DateTimePicker;
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Dialog;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
 
 /**
  * Created by hedhili on 26/05/2016.
  */
-public class CreateEventActivity extends AppCompatActivity {
-
-
+public class CreateEventActivity extends Activity implements DateWatcher {
+    EditText edit_text;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_event
-        );
+        setContentView(R.layout.activity_datetimecustom);
+        edit_text = (EditText) findViewById(R.id.edittext1);
+    }
+
+    public void button_click(View view){
+        // Create the dialog
+        final Dialog mDateTimeDialog = new Dialog(this);
+        // Inflate the root layout
+        final RelativeLayout mDateTimeDialogView = (RelativeLayout) getLayoutInflater().inflate(R.layout.date_time_dialog, null);
+        // Grab widget instance
+        final DateTimePicker mDateTimePicker = (DateTimePicker) mDateTimeDialogView.findViewById(R.id.DateTimePicker);
+        mDateTimePicker.setDateChangedListener(this);
+
+        // Update demo TextViews when the "OK" button is clicked
+        ((Button) mDateTimeDialogView.findViewById(R.id.SetDateTime)).setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                mDateTimePicker.clearFocus();
+                // TODO Auto-generated method stub
+                String result_string = mDateTimePicker.getMonth() + "/" + String.valueOf(mDateTimePicker.getDay()) + "/" + String.valueOf(mDateTimePicker.getYear())
+                        + "  " + String.valueOf(mDateTimePicker.getHour()) + ":" + String.valueOf(mDateTimePicker.getMinute());
+//				if(mDateTimePicker.getHour() > 12) result_string = result_string + "PM";
+//				else result_string = result_string + "AM";
+                edit_text.setText(result_string);
+                mDateTimeDialog.dismiss();
+            }
+        });
+
+        // Cancel the dialog when the "Cancel" button is clicked
+        ((Button) mDateTimeDialogView.findViewById(R.id.CancelDialog)).setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                mDateTimeDialog.cancel();
+            }
+        });
+
+        // Reset Date and Time pickers when the "Reset" button is clicked
+
+        ((Button) mDateTimeDialogView.findViewById(R.id.ResetDateTime)).setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                mDateTimePicker.reset();
+            }
+        });
+
+        // Setup TimePicker
+        // No title on the dialog window
+        mDateTimeDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Set the dialog content view
+        mDateTimeDialog.setContentView(mDateTimeDialogView);
+        // Display the dialog
+        mDateTimeDialog.show();
+    }
+    public void onDateChanged(Calendar c) { // goi khi co thay doi tu calendar
+        Log.e("",
+                "" + c.get(Calendar.MONTH) + " " + c.get(Calendar.DAY_OF_MONTH)
+                        + " " + c.get(Calendar.YEAR));
     }
 }
